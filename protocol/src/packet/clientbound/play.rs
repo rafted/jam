129ebec14082cf5eb37c83f772b757da093bd857
alex <pcranaway@tuta.io>
2023-02-06 15:06:46 +0200

    add join game packet

--- protocol/src/packet/clientbound/play.rs
+++ protocol/src/packet/clientbound/play.rs
@@ -6,3 +6,14 @@ use crate::varint::VarInt;
 pub struct KeepAlivePacket {
     pub id: VarInt,
 }
+
+#[derive(PacketDef)]
+pub struct JoinGamePacket {
+    pub entity_id: i32,
+    pub gamemode: u16,
+    pub dimension: i8,
+    pub difficulty: u8,
+    pub max_players: u8,
+    pub level_type: String,
+    pub reduced_debug_info: bool,
+}
