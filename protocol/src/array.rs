2b77e3ba41fbf73bb2ecdba0894869600c7b0fe9
Patrick <96917554+patrickzondervan@users.noreply.github.com>
2023-02-06 22:34:47 +0100

    remove DynamicArray

--- protocol/src/array.rs
+++ protocol/src/array.rs
@@ -1,13 +1,17 @@
-// Array which we know the size of, so we read x amount of times
-// A: The type of the length/count/size of the array
-// B: The type of the array
+/// A struct to store a length and an array of elements.
+///
+/// # Examples
+///
+/// ```rs
+/// let ca = CountedArray {
+///     len: 5,
+///     arr: vec![1, 2, 3, 4, 5],
+/// };
+///
+/// assert_eq!(ca.len, 5);
+/// assert_eq!(ca.arr, [1, 2, 3, 4, 5]);
+/// ```
 pub struct CountedArray<A, B> {
     pub len: A,
     pub arr: Vec<B>,
-}
-
-// Array we don't know the count of, so we just read as much as we can from the packet
-// this occurs in places where the only field in the packet is the array
-pub struct DynamicArray<A> {
-    pub arr: Vec<A>,
 }
