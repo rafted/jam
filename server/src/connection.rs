5e81244ef62bc99af7c019e8f0660e7cf360cafb
pcranaway <pcranaway@tuta.io>
2023-02-12 00:56:31 +0200

    make base of handle_packet

--- server/src/connection.rs
+++ server/src/connection.rs
@@ -1,5 +1,5 @@
 use bytes::BytesMut;
-use protocol::{encoding::Encodable, state::State, varint::VarInt};
+use protocol::{encoding::Encodable, state::State, varint::VarInt, packet::serverbound::handshaking::HandshakePacket};
 use tokio::{
     io::{AsyncReadExt, Interest},
     net::TcpStream,
@@ -10,7 +10,7 @@ pub struct Connection {
 }
 
 impl Connection {
-    pub async fn handle_loop(self, mut stream: TcpStream) -> anyhow::Result<()> {
+    pub async fn handle_loop(mut self, mut stream: TcpStream) -> anyhow::Result<()> {
         let mut buf = BytesMut::new();
 
         loop {
@@ -27,11 +27,34 @@ impl Connection {
             let _length = VarInt::decode(&mut buf)?;
             let id = VarInt::decode(&mut buf)?;
 
-            self.handle_packet(id.0, &mut buf);
+            self.handle_packet(id.0, &mut buf)?;
         }
     }
 
-    pub fn handle_packet(&self, _id: i32, _buf: &mut BytesMut) -> anyhow::Result<()> {
-        todo!()
+    pub fn handle_packet(&mut self, id: i32, buf: &mut BytesMut) -> anyhow::Result<()> {
+
+        match self.state {
+            State::Handshaking => {
+
+                match id {
+                    0 => {
+                        let packet = HandshakePacket::decode(buf)?;
+
+                        println!("next_state: {}", packet.next_state.0);
+                        println!("port: {}", packet.server_port);
+                        println!("address: {}", packet.server_address);
+                        println!("protocol: {}", packet.protocol_version.0);
+                    }
+
+                    _ => todo!("implement packet")
+                }
+
+            },
+            State::Login => todo!("login state"),
+            State::Play => todo!("play state"),
+            State::Closed => todo!("closed state"),
+        }
+
+        Ok(())
     }
 }
